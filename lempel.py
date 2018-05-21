5af7231857c63830d74ccf82737857c81e7ac730
abedmurrar@hamzah-pc <abedmurrar15@gmail.com>
2018-05-21 10:13:48 +0300

    Initial commit

---
+++ lempel.py
@@ -0,0 +1,21 @@
+import lzw
+import os
+import plotly.plotly as py
+import plotly.graph_objs as go
+print "LZW"
+inputf = lzw.readbytes('uncompressed.txt')
+compressed = lzw.compress(inputf)
+lzw.writebytes('compressed.lzw', compressed)
+ratio = os.stat('uncompressed.txt').st_size/float(os.stat('compressed.lzw').st_size)
+print "Compression Ratio : " ,ratio
+
+os.system("python adaptive-arithmetic-compress.py uncompressed.txt compressed.bin")
+ratio2 = os.stat('uncompressed.txt').st_size/float(os.stat('compressed.bin').st_size)
+print "Adaptive Arithmetic coding"
+print "Compression Ratio : " ,ratio2
+
+labels = ['Lempel-Ziv','arithmetic']
+values = [ratio,ratio2]
+trace = go.Pie(labels=labels, values=values)
+
+py.iplot([trace], filename='abedmurrar15')
